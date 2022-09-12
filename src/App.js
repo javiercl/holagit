4386bdaec2b7e205edfba3f31afced74edcb5e3f
Javier Cisneros <jvr.cisneros@gmail.com>
2022-09-12 15:08:50 -0500

    For Javier

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React For Dummies
+          Learn React For Javier
         </a>
       </header>
     </div>
